47735704bea101946bd4ee172a191d9cf2897195
Shauli Taragin <shaulitaragin@gmail.com>
2021-12-13 22:11:39 +0200

    added main function and gematria

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 #define WORD 30
 #define TXT 1024
+
+static int sumg = 0;
 
 int convertToGem(char c){
     if(c >='a' && c<='z'){
@@ -11,27 +14,29 @@ int convertToGem(char c){
     }
     return 0;
 }
-int User(){
-    char temp[WORD], temp2[TXT];
-    int sumg =0;
+void User(){
+    char words[WORD], paragraph[TXT];
     for (int i = 0; i < WORD; ++i) {
-        scanf("%[^\n]", temp);
-
-        if(temp[i] == '\0'){
+        scanf("%[^\n]", words);
+        if(words[i] == '\0'){
             break;
         }
-        sumg += convertToGem(temp[i]);
+        sumg += convertToGem(words[i]);
     }
-
+    scanf("%[^~]", paragraph);
+    paragraph[strlen(paragraph)]='~';
+    int pointer_for_text = 0;
     for (int i = 0; i < TXT; ++i) {
-        scanf("%[^~]", temp2);
+        int temp [TXT];
     }
-    printf("%s", temp);
-    printf("%s\n", temp2);
-    return sumg;
+    
+
+
+    printf("%s", words);
+    printf("%s\n", paragraph);
 }
 
 void main(){
-    int result = User();
-    printf("the gemtria of the word is: %d\n",result);
+    User();
+    //printf("%d",smug);
 }
